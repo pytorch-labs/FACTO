07eb27fde39613408717daddb35e8b6ac8ac59bb
Manuel Candales <mcandales@meta.com>
2024-01-09 12:15:49 -0800

    InputGen: introduce variable types
    Summary:
    This is the first in a stack of diffs aimed at landing InputGen, a test generation engine for pytorch ops
    
    This diff introduces the most basic variable types that are generated. These are the building blocks for generating more complex structures. These variable types are:
    - Bool (True or False)
    - Int (any integral value)
    - Float (any floating point value)
    - String (any string)
    - ScalarDtype (currently this can take only 3 values: bool, int or float)
    - TensorDtype
    - Tuple (any tuple of values)
    
    Reviewed By: digantdesai
    
    Differential Revision: D52047188

---
+++ test/inputgen/test_variable_types.py
@@ -0,0 +1,65 @@
+import unittest
+
+import torch
+from inputgen.variable.type import (
+    convert_to_vtype,
+    invalid_vtype,
+    is_integer,
+    ScalarDtype,
+    VariableType,
+)
+
+
+class TestVariableType(unittest.TestCase):
+    def test_variable_type(self):
+        self.assertEqual(VariableType.Bool.value, bool)
+        self.assertEqual(VariableType.Int.value, int)
+        self.assertEqual(VariableType.Float.value, float)
+        self.assertEqual(VariableType.String.value, str)
+        self.assertEqual(VariableType.Tuple.value, tuple)
+        self.assertEqual(VariableType.ScalarDtype.value, ScalarDtype)
+        self.assertEqual(VariableType.TensorDtype.value, torch.dtype)
+
+    def test_is_integer(self):
+        self.assertTrue(is_integer(1))
+        self.assertTrue(is_integer(5))
+        self.assertTrue(is_integer(-5))
+        self.assertTrue(is_integer(1.0))
+        self.assertTrue(is_integer(0.0))
+        self.assertTrue(is_integer(-3.0))
+        self.assertTrue(is_integer(True))
+        self.assertTrue(is_integer(False))
+        self.assertFalse(is_integer(3.5))
+        self.assertFalse(is_integer(float("inf")))
+        self.assertFalse(is_integer(float("-inf")))
+        self.assertFalse(is_integer(float("nan")))
+
+    def test_convert(self):
+        self.assertEqual(convert_to_vtype(VariableType.Bool.value, 1.0), True)
+        self.assertEqual(convert_to_vtype(VariableType.Int.value, False), 0)
+        self.assertEqual(convert_to_vtype(VariableType.Float.value, 3), 3.0)
+        self.assertEqual(
+            convert_to_vtype(VariableType.Int.value, float("inf")), float("inf")
+        )
+
+    def test_invalid_vtype(self):
+        self.assertFalse(invalid_vtype(VariableType.Bool.value, 1.0))
+        self.assertFalse(invalid_vtype(VariableType.Int.value, 1.0))
+        self.assertFalse(invalid_vtype(VariableType.Float.value, 1.0))
+        self.assertFalse(invalid_vtype(VariableType.String.value, "hello"))
+        self.assertFalse(invalid_vtype(VariableType.Tuple.value, (1, 2)))
+        self.assertFalse(
+            invalid_vtype(VariableType.ScalarDtype.value, ScalarDtype.bool)
+        )
+        self.assertFalse(invalid_vtype(VariableType.ScalarDtype.value, ScalarDtype.int))
+        self.assertFalse(
+            invalid_vtype(VariableType.ScalarDtype.value, ScalarDtype.float)
+        )
+        self.assertFalse(invalid_vtype(VariableType.TensorDtype.value, torch.bool))
+        self.assertTrue(invalid_vtype(VariableType.Float.value, "1.0"))
+        self.assertTrue(invalid_vtype(VariableType.String.value, 1))
+        self.assertTrue(invalid_vtype(VariableType.ScalarDtype.value, torch.int8))
+
+
+if __name__ == "__main__":
+    unittest.main()
