76b4a6e34f02d4f12f47cc2aa65bad6371e9fcfe
Manuel Candales <mcandales@meta.com>
2024-01-12 12:27:34 -0800

    InputGen: introduce variable space (#3)
    Summary:
    
    This diff introduces the VariableSpace class.
    
    A VariableSpace needs to be initialized with a variable type. Its purpose is to describe a space of values for that variable type. This space can be discrete (like a Python set) or it can be a union of disjoint intervals of real numbers (only for suitable variable types).
    
    Reviewed By: digantdesai
    
    Differential Revision: D51816908

---
+++ inputgen/variable/constants.py
@@ -0,0 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+INT64_MAX = 2**63 - 1
+INT64_MIN = -(2**63)
