652a6fae88f4cc3145fa4a805f99219aa9fdca3a
Manuel Candales <mcandales@meta.com>
2025-08-12 15:17:40 -0400

    Update
    [ghstack-poisoned]

--- test/specdb/test_specdb_cpu.py
+++ test/specdb/test_specdb_cpu.py
@@ -6,27 +6,35 @@
 
 import unittest
 
+from facto.inputgen.utils.config import TensorConfig
+
 from base_test import BaseSpecDBTest
 
 
 class TestSpecDBOperationsCPU(BaseSpecDBTest):
     """Test class for validating all specs in SpecDB using gen_errors on CPU."""
 
-    def test_all_ops_cpu(self):
-        skip_ops = [
-            "_native_batch_norm_legit_no_training.default",
-            "addmm.default",
-            "arange.default",
-            "arange.start_step",
-            "constant_pad_nd.default",
-            "split_with_sizes_copy.default",
-        ]
+    SKIP_OPS = [
+        "_native_batch_norm_legit_no_training.default",
+        "addmm.default",
+        "arange.default",
+        "arange.start_step",
+        "constant_pad_nd.default",
+        "split_with_sizes_copy.default",
+    ]
 
+    def test_all_ops_cpu(self):
+        config = TensorConfig(device="cpu", half_precision=False)
+        self._run_all_ops(config=config, skip_ops=self.SKIP_OPS)
+
+    def test_all_ops_cpu_half(self):
+        skip_ops = self.SKIP_OPS.copy()
         # "cdist" not implemented for 'Half' on CPU
         # "pdist" not implemented for 'Half' on CPU
         skip_ops += ["_cdist_forward.default", "_pdist_forward.default"]
 
-        self._run_all_ops(skip_ops=skip_ops)
+        config = TensorConfig(device="cpu", half_precision=True)
+        self._run_all_ops(config=config, skip_ops=skip_ops)
 
 
 if __name__ == "__main__":
